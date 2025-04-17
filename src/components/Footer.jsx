547d6496e98b6ab410981f11813f155a852a39d1
Jonatha L Teixeira <106690382+jonathatbusiness@users.noreply.github.com>
2025-04-17 12:11:34 -0300

    Google improvments
    Add
    robts.txt
    sitemap.xml
    Fixed some Google SEO settings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,12 @@ const Footer = ({ language }) => {
     <footer className="footer">
       <div className="upper-footer">
         <p>Jonatha Teixeira, 2025.</p>
-        <a href="/privacy-policy" className="privacy-policy-link">
+        <a
+          href="/privacy-policy"
+          className="privacy-policy-link"
+          alt="Read our Privacy Policy"
+          aria-label="Read our Privacy Policy"
+        >
           {t("privacy_footer_link")}
         </a>
       </div>
